b73a941c19e7bf6c4c078707d93568e856a0b58c
Anika Jain <anikajain03@gmail.com>
2024-12-01 14:22:11 -0800

    Minor change to formatting / alignment

--- src/index.js
+++ src/index.js
@@ -1,11 +1,12 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import ReactDOM from 'react-dom/client';
 import './App.css';
 import App from './App';
 
-ReactDOM.render(
+// Create a root and render the app
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
   <React.StrictMode>
     <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+  </React.StrictMode>
 );
